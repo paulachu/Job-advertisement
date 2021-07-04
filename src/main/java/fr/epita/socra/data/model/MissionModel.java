03799d4b29e0b3ba6a1533afaa8085a323e2a30e
Paul Radic <paul.radic@epita.fr>
2021-07-04 03:29:16 +0200

    core(dto): add CreateMissionRequest

--- src/main/java/fr/epita/socra/data/model/MissionModel.java
+++ src/main/java/fr/epita/socra/data/model/MissionModel.java
@@ -1,9 +1,15 @@
 package fr.epita.socra.data.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.smallrye.common.constraint.NotNull;
+import io.smallrye.common.constraint.Nullable;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.Date;
 
 @Entity
 @Table(name = "missions")
@@ -16,4 +22,37 @@ public class MissionModel extends PanacheEntityBase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotNull
+    @Column(nullable = false)
+    private String location;
+
+    @NotNull
+    @Column(nullable = false)
+    private int duration;
+
+    @Nullable
+    @Column(nullable = true)
+    private int price;
+
+    @Column(nullable = false)
+    @PositiveOrZero(message = "Remote should not be negative")
+    @Max(value = 1, message = "Remote should not be greater than 1")
+    private float remote;
+
+    @NotNull
+    @Column(nullable = false)
+    private Date beginning;
+
+    @NotEmpty
+    @Column(nullable = false)
+    private String job;
+
+    @NotEmpty
+    @Column(nullable = false)
+    private String context;
+
+    @NotEmpty
+    @Column(nullable = false)
+    private String description;
 }
