ce15bbc6316d63e252b95d24cf0f86369fbf0805
Valentin Thiron <valentin.thiron@epita.fr>
2021-07-04 15:12:29 +0200

    core(converter): add converterUpdate

--- src/main/java/fr/epita/socra/converter/entitymodel/MissionEntityToUpdateMissionModel.java
+++ src/main/java/fr/epita/socra/converter/entitymodel/MissionEntityToUpdateMissionModel.java
@@ -1,2 +1,24 @@
-package fr.epita.socra.converter.entitymodel;public class MissionEntityToUpdateMissionModel {
+package fr.epita.socra.converter.entitymodel;
+
+import fr.epita.socra.converter.Converter;
+import fr.epita.socra.converter.ConverterUpdate;
+import fr.epita.socra.data.model.MissionModel;
+import fr.epita.socra.domain.entity.MissionEntity;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class MissionEntityToUpdateMissionModel implements ConverterUpdate<MissionEntity, MissionModel> {
+    @Override
+    public MissionModel convertNotNull(MissionEntity from, MissionModel to) {
+        to.setLocation(from.location());
+        to.setDuration(from.duration());
+        to.setPrice(from.price());
+        to.setRemote(from.remote());
+        to.setBeginning(from.beginning());
+        to.setJob(from.job());
+        to.setContext(from.context());
+        to.setDescription(from.description());
+        return to;
+    }
 }
