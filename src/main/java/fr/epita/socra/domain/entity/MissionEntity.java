add226e7274e25ff31695f68c527b4409226b1ba
Mehdi Seddiki <mehdi.seddiki@epita.fr>
2021-07-04 03:46:47 +0200

    core(entity): implement MissionEntity

--- src/main/java/fr/epita/socra/domain/entity/MissionEntity.java
+++ src/main/java/fr/epita/socra/domain/entity/MissionEntity.java
@@ -2,5 +2,14 @@ package fr.epita.socra.domain.entity;
 
 import java.util.Date;
 
-public record MissionEntity() {
+public record MissionEntity(
+        long id,
+        String location,
+        int duration,
+        int price,
+        float remote,
+        Date beginning,
+        String job,
+        String context,
+        String description) {
 }
