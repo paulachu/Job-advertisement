1bab29d446140adde5214959a58afef18af271d1
Valentin Thiron <valentin.thiron@epita.fr>
2021-07-04 14:44:32 +0200

    fix(test service): fix inject service in test

--- src/test/java/fr/epita/socra/MissionServiceTest.java
+++ src/test/java/fr/epita/socra/MissionServiceTest.java
@@ -1,16 +1,15 @@
 package fr.epita.socra;
 
-import fr.epita.socra.converter.Converter;
 import fr.epita.socra.converter.entitymodel.MissionEntityToMissionModel;
 import fr.epita.socra.converter.entitymodel.MissionModelToMissionEntity;
-import fr.epita.socra.data.model.MissionModel;
+import fr.epita.socra.data.repository.MissionRepository;
 import fr.epita.socra.domain.entity.MissionEntity;
 import fr.epita.socra.domain.service.MissionService;
-import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
-import org.junit.jupiter.api.BeforeAll;
+import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.inject.Inject;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -20,11 +19,11 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@QuarkusTest
 class MissionServiceTest {
-    private Converter<MissionModel, MissionEntity> missionModelToMissionEntityConverter= new MissionModelToMissionEntity();
-    private Converter<MissionEntity, MissionModel> missionEntityToMissionModelConverter = new MissionEntityToMissionModel();
-    private List<MissionModel> missionListMock= new ArrayList<>();
-    private MissionService missionService = new MissionService();
+    private List<MissionEntity> missionListMock= new ArrayList<>();
+    @Inject
+    MissionService missionService;
 
     @BeforeEach
     public void initMisisonList()
@@ -46,9 +45,9 @@ class MissionServiceTest {
         var job = "Développeur ReactJs";
         var context = "intégration au sein de l’équipe Engineering du pôle Industrialisation, Cloud and Data, le consultant contribuera aux activités APIs et API Management.";
         var description = "Participer aux Comités d’Architecture pour garantir la bonne conformité des bonnes pratiques des APIs. Promouvoir les pratiques API First au sein du groupe. Rédiger / Maintenir un Guideline de développement d’API (créationde modèle d’API, ...)";
-        MissionModel missionModel = new MissionModel(id, location, duration, price, remote, beginning, job, context, description);
+        MissionEntity missionEntity = new MissionEntity(id, location, duration, price, remote, beginning, job, context, description);
 
-        this.missionListMock.add(missionModel);
+        this.missionListMock.add(missionEntity);
 
         id = 2;
         location = "Le Kremlin-Bicêtre";
@@ -64,9 +63,9 @@ class MissionServiceTest {
         job = "Stagiaire développeur Java";
         context = "Dans la démarche de refonte de leur SI, EPITA a besoin d'une nouvelle application";
         description = "Création d'une application permettant de recenser des missions d'une entreprise, tout en utilisant les bonnes pratiques (Clean code, architecture, tests, CI ....). Création d'une API Rest";
-        missionModel = new MissionModel(id, location, duration, price, remote, beginning, job, context, description);
+        missionEntity = new MissionEntity(id, location, duration, price, remote, beginning, job, context, description);
 
-        this.missionListMock.add(missionModel);
+        this.missionListMock.add(missionEntity);
 
         id = 3;
         location = "Sancerre";
@@ -82,9 +81,9 @@ class MissionServiceTest {
         job = "Web Designer";
         context = "Ayant gagner la compétition du plus beau village français, Sancerre souhaite créer une application mobile pour valoriser son patrimoine";
         description = "Création de maquettes Adobe XD d'une application mobile permettant de recenser les différents lieux touristiques du village";
-        missionModel = new MissionModel(id, location, duration, price, remote, beginning, job, context, description);
+        missionEntity = new MissionEntity(id, location, duration, price, remote, beginning, job, context, description);
 
-        this.missionListMock.add(missionModel);
+        this.missionListMock.add(missionEntity);
 
         id = 4;
         location = "Bordeaux";
@@ -100,9 +99,9 @@ class MissionServiceTest {
         job = "Stagiaire développeur ReactJs";
         context = "Souhaitant augmenter sa visibilité, le Château Lafon-Rochet souhaite créer un site e-commerce";
         description = "Création du frontend d'une application permettant de commander des bouteilles ou des caisse de vin, la partie backend sera réalisée dans le même temps par un autre stagiaire";
-        missionModel = new MissionModel(id, location, duration, price, remote, beginning, job, context, description);
+        missionEntity = new MissionEntity(id, location, duration, price, remote, beginning, job, context, description);
 
-        this.missionListMock.add(missionModel);
+        this.missionListMock.add(missionEntity);
 
         id = 5;
         location = "Paris";
@@ -118,9 +117,9 @@ class MissionServiceTest {
         job = "Développeur Android Kotlin";
         context = "Dans la cadre de la réalisation d'une application permettant de visualiser les photos prises par les différents rovers, la Nasa a besoin d'une application Android";
         description = "Création d'une application permettant de visualiser les photos prises par les rovers, une API est déjà existante";
-        missionModel = new MissionModel(id, location, duration, price, remote, beginning, job, context, description);
+        missionEntity = new MissionEntity(id, location, duration, price, remote, beginning, job, context, description);
 
-        this.missionListMock.add(missionModel);
+        this.missionListMock.add(missionEntity);
 
         id = 6;
         location = "Belgrade";
@@ -136,9 +135,9 @@ class MissionServiceTest {
         job = "Développeur Wix";
         context = "Pour la refonte de leur site vitrine, VIP Mobile a besoin d'un nouveau frontend";
         description = "Création d'un frontend permettant de visualiser les produits et les différentes annonces de l'entreprise";
-        missionModel = new MissionModel(id, location, duration, price, remote, beginning, job, context, description);
+        missionEntity = new MissionEntity(id, location, duration, price, remote, beginning, job, context, description);
 
-        this.missionListMock.add(missionModel);
+        this.missionListMock.add(missionEntity);
 
         id = 7;
         location = "Barcelonnette";
@@ -154,9 +153,9 @@ class MissionServiceTest {
         job = "Rédacteur";
         context = "Ayant pour devoir la rédaction d'un rapport sur le bouquetin des Alpes, un élève de l'Epita est à la recherche d'un rédacteur";
         description = "Rédaction d'un mémoire de 50 pages sur les bouquetins des Alpes, le but étant d'étudier leur comportement";
-        missionModel = new MissionModel(id, location, duration, price, remote, beginning, job, context, description);
+        missionEntity = new MissionEntity(id, location, duration, price, remote, beginning, job, context, description);
 
-        this.missionListMock.add(missionModel);
+        this.missionListMock.add(missionEntity);
 
         id = 8;
         location = "Le Kremlin-Bicêtre";
@@ -172,9 +171,9 @@ class MissionServiceTest {
         job = "Stagiaire développeur Java";
         context = "Dans le cadre de la production d'une nouvelle solution l'entreprise Uber a besoin d'une nouvelle application";
         description = "Création d'une application permettant de livrer des oeuvres d'art, Uber a beosin d'une application web Java utilisant quarkus et vaadin";
-        missionModel = new MissionModel(id, location, duration, price, remote, beginning, job, context, description);
+        missionEntity = new MissionEntity(id, location, duration, price, remote, beginning, job, context, description);
 
-        this.missionListMock.add(missionModel);
+        this.missionListMock.add(missionEntity);
 
         id = 9;
         location = "Montreuil";
@@ -190,9 +189,9 @@ class MissionServiceTest {
         job = "Stagiaire développeur Java";
         context = "Afin d'améliorer les compétences de leurs équipes, l'entreprise Ubisoft recherche un formateur";
         description = "Pour développer les compétences de leurs équipes, l'entreprise Ubisoft recherche un formateur experimenté dans les moteurs de jeu. Une première formation consistera à expliquer aux développeurs comment exécuter une testsuite";
-        missionModel = new MissionModel(id, location, duration, price, remote, beginning, job, context, description);
+        missionEntity = new MissionEntity(id, location, duration, price, remote, beginning, job, context, description);
 
-        this.missionListMock.add(missionModel);
+        this.missionListMock.add(missionEntity);
 
         id = 10;
         location = "Marseille";
@@ -208,16 +207,16 @@ class MissionServiceTest {
         job = "Stagiaire chargée de communication";
         context = "Afin de redorer son image, l'entreprise Harmoniks a besoin d'une personne chargée de communication";
         description = "L'entreprise Harmoniks ayant connu une année catastrophique, elle cherche à redorer son image. Une campagne publicitaire est déjà prévue. Cependant, aucune communication digitale n'est actuellement faite.";
-        missionModel = new MissionModel(id, location, duration, price, remote, beginning, job, context, description);
+        missionEntity = new MissionEntity(id, location, duration, price, remote, beginning, job, context, description);
 
-        this.missionListMock.add(missionModel);
+        this.missionListMock.add(missionEntity);
     }
 
     @Test
     void checkFindAllMisisons()
     {
         var res = missionService.findAllMission();
-        Collections.sort(missionListMock, (m1, m2) -> (int) (m1.getId() - m2.getId()));
+        Collections.sort(missionListMock, (m1, m2) -> (int) (m1.id() - m2.id()));
         assertEquals(missionListMock, res);
     }
     @Test
